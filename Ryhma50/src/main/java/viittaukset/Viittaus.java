f284a6bb1c8e45454233ffeaed01e03e6ca5aa2a
Ola <ola.lansman@helsinki.fi>
2016-04-11 14:30:18 +0300

    Lisätty viittaukset

--- Ryhma50/src/main/java/viittaukset/Viittaus.java
+++ Ryhma50/src/main/java/viittaukset/Viittaus.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package viittaukset;
+
+import java.util.*;
 
 /**
  *
  * @author Studies
  */
-public class Viittaus {
+public abstract class Viittaus {
+    protected final HashMap<String, String> latexFields;
+    protected final String TITLE = "title";
+    protected final String AUTHOR = "author";
+    protected final String YEAR= "year";
+    
+    public Viittaus() {
+        this.latexFields = new HashMap<>();
+        this.latexFields.put(TITLE, "");
+        this.latexFields.put(AUTHOR, "");
+        this.latexFields.put(YEAR, "");
+    }
+    
+    public Map returnFields() {
+        return this.latexFields;
+    }
+    
+    public Set returnHeaders() {
+        return this.latexFields.keySet();
+    }
+    
+    public void saveAll(HashMap<String, String> fields) {
+        this.latexFields.put(TITLE, fields.get(TITLE));
+        this.latexFields.put(AUTHOR, fields.get(AUTHOR));
+        this.latexFields.put(YEAR, fields.get(YEAR));
+    }
     
 }
